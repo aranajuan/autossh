511c7b29e5c62e2957825dd558fd247667eb30af
Juan Arana <arana.juanpablo@gmail.com>
2021-06-15 20:32:41 -0300

    Added ExitOnForwardFailure to ssh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ssh-keyscan -H "${REMOTE_HOST}" >>~/.ssh/known_hosts
 
-echo -e $TUNNEL_ARGS | xargs sh -c 'autossh -M 0 -i /root/id_rsa -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -N -T "${@}" "${REMOTE_USER}"@"${REMOTE_HOST}"' _
+echo -e $TUNNEL_ARGS | xargs sh -c 'autossh -M 0 -i /root/id_rsa -o ServerAliveInterval=60 -o ExitOnForwardFailure=true -o ServerAliveCountMax=10 -N -T "${@}" "${REMOTE_USER}"@"${REMOTE_HOST}"' _
 
 echo "Running"
